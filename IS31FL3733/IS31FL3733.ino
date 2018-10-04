a3356d70331689ea57cb1f309f913229b3f62ef0
LT11222 <luke11222@gmail.com>
2018-10-04 02:20:55 -0400

    Four color changing RGB LEDs

--- IS31FL3733/IS31FL3733.ino
+++ IS31FL3733/IS31FL3733.ino
@@ -22,37 +22,72 @@ void setup()
     device.init();
     device.setCurrent();
 
-    device.led_state(0x00, 0x01); //red
-    device.led_state(0x02, 0x01); //green
-    device.led_state(0x04, 0x01); //blue
+    device.led_state(0x00, 0x03); //red
+    device.led_state(0x02, 0x03); //green
+    device.led_state(0x04, 0x03); //blue
+
+    device.led_state(0x06, 0x03); //red
+    device.led_state(0x08, 0x03); //green
+    device.led_state(0x0A, 0x03); //blue
 
     device.led_pwm(0x00, 255); //red
-    // device.led_pwm(0x10, 255); //green
-    // device.led_pwm(0x20, 255); //blue
+    device.led_pwm(0x30, 255); //red
+    device.led_pwm(0x01, 255); //red
+    device.led_pwm(0x31, 255); //red
+
+    device.led_pwm(0x10, 255); //green
+    device.led_pwm(0x40, 255); //green
+    device.led_pwm(0x11, 255); //green
+    device.led_pwm(0x41, 255); //green
+
+    device.led_pwm(0x20, 255); //blue
+    device.led_pwm(0x50, 255); //blue
+    device.led_pwm(0x21, 255); //blue
+    device.led_pwm(0x51, 255); //blue
+
+    delay(1000);
 }
 
 uint32_t prevTime;
 
 void loop()
 {
-    for (int i = 0; i < 255; i++)
+    for (int i = 0; i < 256; i++)
     {
         device.led_pwm(0x00, 255 - i); //red
         device.led_pwm(0x10, i);       //green
+        device.led_pwm(0x30, 255 - i); //red
+        device.led_pwm(0x40, i);       //green
+        device.led_pwm(0x01, 255 - i); //red
+        device.led_pwm(0x11, i);       //green
+        device.led_pwm(0x31, 255 - i); //red
+        device.led_pwm(0x41, i);       //green
         delay(10);
     }
 
-    for (int i = 0; i < 255; i++)
+    for (int i = 0; i < 256; i++)
     {
         device.led_pwm(0x10, 255 - i); //green
         device.led_pwm(0x20, i);       //blue
+        device.led_pwm(0x40, 255 - i); //green
+        device.led_pwm(0x50, i);       //blue
+        device.led_pwm(0x11, 255 - i); //green
+        device.led_pwm(0x21, i);       //blue
+        device.led_pwm(0x41, 255 - i); //green
+        device.led_pwm(0x51, i);       //blue
         delay(10);
     }
 
-    for (int i = 0; i < 255; i++)
+    for (int i = 0; i < 256; i++)
     {
         device.led_pwm(0x20, 255 - i); //blue
         device.led_pwm(0x00, i);       //red
+        device.led_pwm(0x50, 255 - i); //blue
+        device.led_pwm(0x30, i);       //red
+        device.led_pwm(0x21, 255 - i); //blue
+        device.led_pwm(0x01, i);       //red
+        device.led_pwm(0x51, 255 - i); //blue
+        device.led_pwm(0x31, i);       //red
         delay(10);
     }
 }

f3add7f2c43bbcbf662f1fc476764b6562e96713
LT11222 <LT11222@users.noreply.github.com>
2021-12-12 14:31:19 -0500

    Updates

--- IS31FL3733/IS31FL3733.ino
+++ IS31FL3733/IS31FL3733.ino
@@ -5,20 +5,20 @@ IS31FL3733 device;
 
 void setup()
 {
-    Serial.begin(9600);
+    // Serial.begin(9600);
 
-    while (!Serial)
-    {
-    }
+    // while (!Serial)
+    // {
+    // }
 
     Wire.begin();
 
     pinMode(5, OUTPUT);
     digitalWrite(5, HIGH);
 
-    Serial.println("STARTED");
+    // Serial.println("STARTED");
 
-    Serial.println(device.checkConnectivity());
+    // Serial.println(device.checkConnectivity());
     device.init();
     device.setCurrent();
 
@@ -52,42 +52,126 @@ uint32_t prevTime;
 
 void loop()
 {
+    // for (int i = 0; i < 256; i++)
+    // {
+    //     device.led_pwm(0x00, 255 - i); //red
+    //     device.led_pwm(0x10, i);       //green
+    //     device.led_pwm(0x30, 255 - i); //red
+    //     device.led_pwm(0x40, i);       //green
+    //     device.led_pwm(0x01, 255 - i); //red
+    //     device.led_pwm(0x11, i);       //green
+    //     device.led_pwm(0x31, 255 - i); //red
+    //     device.led_pwm(0x41, i);       //green
+    //     delay(1);
+    // }
+
+    // for (int i = 0; i < 256; i++)
+    // {
+    //     device.led_pwm(0x10, 255 - i); //green
+    //     device.led_pwm(0x20, i);       //blue
+    //     device.led_pwm(0x40, 255 - i); //green
+    //     device.led_pwm(0x50, i);       //blue
+    //     device.led_pwm(0x11, 255 - i); //green
+    //     device.led_pwm(0x21, i);       //blue
+    //     device.led_pwm(0x41, 255 - i); //green
+    //     device.led_pwm(0x51, i);       //blue
+    //     delay(1);
+    // }
+
+    // for (int i = 0; i < 256; i++)
+    // {
+    //     device.led_pwm(0x20, 255 - i); //blue
+    //     device.led_pwm(0x00, i);       //red
+    //     device.led_pwm(0x50, 255 - i); //blue
+    //     device.led_pwm(0x30, i);       //red
+    //     device.led_pwm(0x21, 255 - i); //blue
+    //     device.led_pwm(0x01, i);       //red
+    //     device.led_pwm(0x51, 255 - i); //blue
+    //     device.led_pwm(0x31, i);       //red
+    //     delay(1);
+    // }
+
     for (int i = 0; i < 256; i++)
     {
         device.led_pwm(0x00, 255 - i); //red
         device.led_pwm(0x10, i);       //green
-        device.led_pwm(0x30, 255 - i); //red
-        device.led_pwm(0x40, i);       //green
-        device.led_pwm(0x01, 255 - i); //red
-        device.led_pwm(0x11, i);       //green
-        device.led_pwm(0x31, 255 - i); //red
-        device.led_pwm(0x41, i);       //green
-        delay(10);
+        delay(1);
     }
 
     for (int i = 0; i < 256; i++)
     {
         device.led_pwm(0x10, 255 - i); //green
         device.led_pwm(0x20, i);       //blue
-        device.led_pwm(0x40, 255 - i); //green
-        device.led_pwm(0x50, i);       //blue
-        device.led_pwm(0x11, 255 - i); //green
-        device.led_pwm(0x21, i);       //blue
-        device.led_pwm(0x41, 255 - i); //green
-        device.led_pwm(0x51, i);       //blue
-        delay(10);
+        delay(1);
     }
 
     for (int i = 0; i < 256; i++)
     {
         device.led_pwm(0x20, 255 - i); //blue
         device.led_pwm(0x00, i);       //red
+        delay(1);
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        device.led_pwm(0x30, 255 - i); //red
+        device.led_pwm(0x40, i);       //green
+        delay(1);
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        device.led_pwm(0x40, 255 - i); //green
+        device.led_pwm(0x50, i);       //blue
+        delay(1);
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
         device.led_pwm(0x50, 255 - i); //blue
         device.led_pwm(0x30, i);       //red
+        delay(1);
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        device.led_pwm(0x01, 255 - i); //red
+        device.led_pwm(0x11, i);       //green
+        delay(1);
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        device.led_pwm(0x11, 255 - i); //green
+        device.led_pwm(0x21, i);       //blue
+        delay(1);
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
         device.led_pwm(0x21, 255 - i); //blue
         device.led_pwm(0x01, i);       //red
+        delay(1);
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        device.led_pwm(0x31, 255 - i); //red
+        device.led_pwm(0x41, i);       //green
+        delay(1);
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        device.led_pwm(0x41, 255 - i); //green
+        device.led_pwm(0x51, i);       //blue
+        delay(1);
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
         device.led_pwm(0x51, 255 - i); //blue
         device.led_pwm(0x31, i);       //red
-        delay(10);
-    }
+        delay(1);
+    }    
 }
